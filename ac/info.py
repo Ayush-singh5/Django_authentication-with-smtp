e3a34d461e4a4c4f9bb4ac089089e9f2de4af349
Ayush-singh5 <134785298+Ayush-singh5@users.noreply.github.com>
2024-05-30 00:38:33 +0530

    Update info.py

--- ac/info.py
+++ ac/info.py
@@ -1,5 +1,5 @@
 EMAIL_USER_TLS=True
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER='djangoacproject@gmail.com'
-EMAIL_HOST_PASSWORD='tvzy syhz eqoc ghyw'
+EMAIL_HOST_USER='your email here'
+EMAIL_HOST_PASSWORD='the app password here'
 EMAIL_PORT=587
